66d5f94c8038f71808fcbfd2076c41c9b986ee93
Bipul Chaudhary <bipulchau@gmail.com>
2022-06-27 00:05:29 -0400

    Solution q/no 2

--- select-from-world.sql
+++ select-from-world.sql
@@ -1,3 +1,4 @@
+-- Introduction
 -- 1 Observe the result of running this SQL command to show the name,
 -- continent
 -- and population of all countries.
@@ -6,4 +7,12 @@ SELECT
   continent,
   population
 FROM
+  world -- Large Countries
+  -- Show the name for the countries that have a population of at least 200 million.
+  -- 200 million is 200000000,there are eight zeros.
+SELECT
+  name
+FROM
   world
+WHERE
+  population >= 200000000
